8f4dbc70faf0d6d4a0f34dfdd70b030efed85dce
Fabio Tramasoli <fabio@tramasoli.com>
2019-11-10 12:45:27 -0300

    crud com CBV

--- minhaapp/urls.py
+++ minhaapp/urls.py
@@ -4,9 +4,9 @@ from minhaapp import views
 
 urlpatterns = [
     path('', views.index),
-    path('new', views.PresencaView.as_view(), name='presenca_new'),
-    path('view/<int:pk>', views.PresencaView.as_view(), name='presenca_view'),
-    path('edit/<int:pk>', views.PresencaView.as_view(), name='presenca_edit'),
-    path('delete/<int:pk>', views.PresencaView.as_view(), name='presenca_delete'),
+    path('new', views.CriaPresencaView.as_view(), name='presenca_new'),
+    path('view/<int:pk>', views.DetailPresencaView.as_view(), name='presenca_view'),
+    path('edit/<int:pk>', views.EditPresencaView.as_view(), name='presenca_edit'),
+    path('delete/<int:pk>', views.DeletePresencaView.as_view(), name='presenca_delete'),
     path('list', views.ListaPresencaView.as_view(), name='presenca_list'),
 ]
--- minhaapp/views.py
+++ minhaapp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView, DetailView
 
 from minhaapp.forms import PresencaForm
 from minhaapp.models import Presenca
@@ -14,8 +14,26 @@ def index(request):
 class ListaPresencaView(ListView):
     model = Presenca
 
-class PresencaView(CreateView):
+class CriaPresencaView(CreateView):
     template_name = 'minhaapp/presenca.html'
     form_class = PresencaForm
     # success_url = reverse_lazy('presenca_list')
     success_url = '/minhaapp/list'
+
+
+class EditPresencaView(UpdateView):
+    template_name = 'minhaapp/presenca.html'
+    model = Presenca
+    fields = '__all__'
+    # success_url = reverse_lazy('presenca_list')
+    success_url = '/minhaapp/list'
+
+
+class DeletePresencaView(DeleteView):
+    model = Presenca
+    # success_url = reverse_lazy('presenca_list')
+    success_url = '/minhaapp/list'
+
+
+class DetailPresencaView(DetailView):
+    model = Presenca
