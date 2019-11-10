f0f5341b64887b29192586440fbc55f5101c39be
Fabio Tramasoli <fabio@tramasoli.com>
2019-11-10 13:13:00 -0300

    validações no modelo

--- minhaapp/models.py
+++ minhaapp/models.py
@@ -1,4 +1,8 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from datetime import date
 
 # Create your models here.
 
@@ -6,3 +10,9 @@ class Presenca(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(null=False, blank=False, max_length=100)
     date = models.DateField(null=False, blank=False, auto_now=False, auto_now_add=False)
+
+    def save(self, *args, **kwargs):
+        hoje = date.today()
+        if self.pk is None and self.date != hoje:
+            raise ValidationError(_('Data da presença tem de ser hoje.'))
+        super(Presenca, self).save(*args, **kwargs)
