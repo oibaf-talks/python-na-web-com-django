799dad6c219caab297fc6731e2d7a1a98a45f371
Fabio Tramasoli <fabio@tramasoli.com>
2019-11-10 12:15:06 -0300

    admin crud

--- minhaapp/models.py
+++ minhaapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Presenca(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(null=False, blank=False, max_length=100)
+    date = models.DateField(null=False, blank=False, auto_now=False, auto_now_add=False)
--- minhaapp/views.py
+++ minhaapp/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.shortcuts import render
+# from django.urls import reverse_lazy
+from django.views.generic import FormView
+
+from minhaapp.forms import PresencaForm
 
 # Create your views here.
 
 def index(request):
     return HttpResponse('yey')
+
+class ContactView(FormView):
+    template_name = 'minhaapp/presenca.html'
+    form_class = PresencaForm
+    # success_url = reverse_lazy('site.index')
+    success_url = '/'
